067953657596d4407e0dc998327e51ca3114700b
Neeju Devaraju <Neeju Devaraju@DESKTOP-N3G62FT>
2025-05-22 17:02:24 +0530

    commit changes in aggregate

--- Aggregation_Java/src/main/java/java_Aggregate/Aggregate.java
+++ Aggregation_Java/src/main/java/java_Aggregate/Aggregate.java
@@ -6,7 +6,14 @@ public class Aggregate {
 		College cg = new College("BARTON HILL","TRIVANDRUM");
 		Department dp=new Department("MECHANICAL",465,"JOSEPH");
         Student s= new Student("henry",26,dp,cg);
-      s.display();
+        s.display();//first student
+        System.out.println();
+       Student s1=new Student("James",01,dp,cg);//object to call second student data
+       s1.display();
+       System.out.println();
+       Department dp1=new Department("Civil engineering",466,"REKHA");//object for a different department
+       Student s3=new Student("POOJA",03,dp1,cg);
+       s3.display();
 	}
 
 }
